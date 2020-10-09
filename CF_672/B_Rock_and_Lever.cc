249006c26a4e929ac9b0aa05491630ef770c2fd0
Kvar_ispw17 <enkerewpo@gmail.com>
2020-10-09 08:57:33 +0800

    Kv

--- CF_672/B_Rock_and_Lever.cc
+++ CF_672/B_Rock_and_Lever.cc
@@ -20,6 +20,27 @@ void read(_Tp &a, char c = 0) {
 	for (a = 0; isdigit(c); a = a * 10 + c - '0', c = getchar());
 }
 
+const int N = 1e5 + 10;
+
+int T, n;
+int a[N];
+
+
 int main() {
+	read(T);
+	while (T--) {
+		memset(a, 0, sizeof a);
+		read(n);
+		for (int i = 1; i <= n; i++) read(a[i]);
+		ll ans = 0;
+		for (int j = 31; j >= 0; j--) {
+			ll cnt = 0;
+			for (int i = 1; i <= n; i++) {
+				if (a[i] >= (1 << j) && a[i] < (1 << (j + 1))) cnt++;
+			}
+			ans += cnt * (cnt - 1) / 2;
+		}
+		printf("%lld\n", ans);
+	}
 	return 0;
 }
